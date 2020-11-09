9847ca1cef6eaf62f1ede65ea60db5e951af07c4
purwokang <purwo.com@gmail.com>
2020-11-09 20:51:28 +0700

    comment result

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let quoteSample = "The quick brown fox jumps over the lazy dog.";
 let alphabetRegex = /[a-z]/gi; // Change this line
 let result = quoteSample.match(alphabetRegex); // Change this line
-console.log(result);
+// console.log(result);
 console.log(result.sort());
 
 // let quoteSample = "The five boxing wizards jump quickly.";
